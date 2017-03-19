ff3c45b03889964f7b279b5773676f31f2ead1bd
JosseKormelink <jossekormelink@hotmail.com>
2017-03-19 19:23:24 +0100

    Added remove funtionality to events page. Alerts not working.

--- src/app/evenementen/evenementen.component.ts
+++ src/app/evenementen/evenementen.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { IAlert } from './../model/alert';
 
 @Component({
   selector: 'dzwelg-evenementen',
@@ -9,11 +10,34 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class EvenementenComponent implements OnInit {
 
   evenementen: FirebaseListObservable<any[]>;
+  alerts: IAlert[];
 
   constructor(private af: AngularFire) { }
 
   ngOnInit() {
     this.evenementen = this.af.database.list('evenementen');
+  }
+
+  verwijder(id: string) {
+    this.evenementen.remove(id).then(
+      succes => {
+        this.alerts.push({
+          type: 'succes',
+          message: 'Het evenement werd succesvol verwijderd.'
+        });
+      },
+      error => {
+        this.alerts.push({
+          type: 'danger',
+          message: 'Het evenement kon niet worden verwijderd'
+        });
+      }
+    );
+  }
+
+  public sluitAlert(alert: IAlert) {
+    const index: number = this.alerts.indexOf(alert);
+    this.alerts.splice(index, 1);
   }
 
 }
