b2ecb0ebfef100e6948257d20db4c5fb90549efd
JosseKormelink <jossekormelink@hotmail.com>
2017-03-19 16:40:32 +0100

    Added Angularfire2 in RootModule + changed tslint component prefix.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCntxaPSHYLkVIEyZEkUwMKlU6frX2ZLlQ',
+  authDomain: 'dzwelg-dev.firebaseapp.com',
+  databaseURL: 'https://dzwelg-dev.firebaseio.com',
+  storageBucket: 'dzwelg-dev.appspot.com',
+  messagingSenderId: '258825135187'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, 'dzwelg-web')
   ],
   providers: [],
   bootstrap: [AppComponent]
