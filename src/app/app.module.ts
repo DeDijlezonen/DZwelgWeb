6cb98ca72e267090fb9ba1bf5a9f449d390886f4
JosseKormelink <jossekormelink@hotmail.com>
2017-03-19 17:01:14 +0100

    Added routing and template and events components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+import { EvenementenComponent } from './evenementen/evenementen.component';
+import { PaginaNietGevondenComponent } from './pagina-niet-gevonden/pagina-niet-gevonden.component';
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyCntxaPSHYLkVIEyZEkUwMKlU6frX2ZLlQ',
@@ -13,9 +17,17 @@ export const firebaseConfig = {
   messagingSenderId: '258825135187'
 };
 
+const routes: Routes = [
+  { path: '', component: TemplateComponent, children: [
+    { path: 'evenementen', component: EvenementenComponent },
+    { path: '', redirectTo: '/evenementen', pathMatch: 'full' },
+  ] },
+  { path: '**', component: PaginaNietGevondenComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, TemplateComponent, EvenementenComponent, PaginaNietGevondenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/evenementen/evenementen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dzwelg-evenementen',
+  templateUrl: './evenementen.component.html',
+  styleUrls: ['./evenementen.component.css']
+})
+export class EvenementenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
