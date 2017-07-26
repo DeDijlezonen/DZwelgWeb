4a9a8186ff6c1129fda526b254c03f0cf22e7d66
Wim Hambrouck <wim.hambrouck@gmail.com>
2017-07-26 22:49:03 +0200

    Eigen validatiemessages in FormHelper

--- src/app/utils/functions.ts
+++ src/app/utils/functions.ts
@@ -1,23 +1,38 @@
-import { FormGroup } from '@angular/forms';
+import {FormGroup} from '@angular/forms';
 import * as _ from 'lodash';
 
 export class FormHelper {
 
-    static getFormErrorMessage(formGroup: FormGroup) {
-        let foutBoodschap = '';
-        const controls = formGroup.controls;
-        _.keys(controls).forEach((control_key) => {
-          const errors = controls[control_key].errors;
-          if (errors) {
-            foutBoodschap += control_key + ': ';
-            _.keys(errors).forEach((error_key: string, index) => {
-              foutBoodschap += (error_key.toString() + (_.lastIndexOf(errors) === index ? '' : '; '));
-            });
-          }
-        });
-
-        return foutBoodschap;
+  static validationMessages = {
+    'voornaam': {
+      'required': 'Voornaam is verplicht.',
+    },
+    'achternaam': {
+      'required': 'Achternaam is verplicht.',
+    },
+    'email': {
+      'required': 'E-mail is required.',
+      'email': 'Help, mijn e-mail is geen e-mail!'
     }
+  };
+
+  static getFormErrorMessage(formGroup: FormGroup) {
+    let foutBoodschap = '';
+    const controls = formGroup.controls;
+    _.keys(controls).forEach((control_key) => {
+      const errors = controls[control_key].errors;
+      if (errors) {
+        // foutBoodschap += control_key + ': ';
+        _.keys(errors).forEach((error_key: string, index) => {
+          // foutBoodschap += (error_key.toString() + (_.lastIndexOf(errors) === index ? '' : '; '));
+          foutBoodschap += FormHelper.validationMessages[control_key][error_key] + ' ';
+        });
+      }
+    });
+
+    return foutBoodschap;
+  }
+
 
 }
 
