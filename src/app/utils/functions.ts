95120332980ef0f321776a6b8e38f43289c284c3
Wim Hambrouck <wim.hambrouck@gmail.com>
2017-08-02 19:24:55 +0200

    v2.2.4: verbetering van messages (newline werkt)

--- src/app/utils/functions.ts
+++ src/app/utils/functions.ts
@@ -26,20 +26,20 @@ export class FormHelper {
   };
 
   static getFormErrorMessage(formGroup: FormGroup) {
-    let foutBoodschap = '';
-    const controls = formGroup.controls;
-    _.keys(controls).forEach((control_key) => {
-      const errors = controls[control_key].errors;
-      if (errors) {
-        // foutBoodschap += control_key + ': ';
-        _.keys(errors).forEach((error_key: string, index) => {
-          // foutBoodschap += (error_key.toString() + (_.lastIndexOf(errors) === index ? '' : '; '));
-          foutBoodschap += FormHelper.validationMessages[control_key][error_key] + ' ';
-        });
-      }
-    });
+      let foutBoodschap = '';
+      const controls = formGroup.controls;
+      _.keys(controls).forEach((control_key) => {
+        const errors = controls[control_key].errors;
+        if (errors) {
+          // foutBoodschap += control_key + ': ';
+          _.keys(errors).forEach((error_key: string, index) => {
+            // foutBoodschap += (error_key.toString() + (_.lastIndexOf(errors) === index ? '' : '; '));
+            foutBoodschap += FormHelper.validationMessages[control_key][error_key] + '_';
+          });
+        }
+      });
 
-    return foutBoodschap;
+    return foutBoodschap.substring(0, foutBoodschap.length - 1);
   }
 
 
