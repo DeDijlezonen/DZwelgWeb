ababb4415273300a6f524519abb8bfc314ffc2b5
JosseKormelink <kormelink.josse@gmail.com>
2017-08-06 17:53:43 +0200

    Je kan nu gebruikers in de inschrijftabel zetten.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import { ArraySorteerPipe } from './pipes/array-sorteer-pipe/array-sorteer.pipe'
 import {firebaseConfig} from "../environments/environment";
 import { ActiviteitAanmakenComponent } from './activiteit-aanmaken/activiteit-aanmaken.component';
 import { EvenementenBewerkenComponent } from './evenementen-bewerken/evenementen-bewerken.component';
-import { SorteerReedsIngeschrevenenPipe } from './pipes/sorteer-reeds-ingeschrevenen.pipe';
 
 const routes: Routes = [
   { path: '', component: TemplateComponent, children: [
@@ -53,7 +52,6 @@ const routes: Routes = [
     ArraySorteerPipe,
     ActiviteitAanmakenComponent,
     EvenementenBewerkenComponent,
-    SorteerReedsIngeschrevenenPipe,
   ],
   imports: [
     NgbModule.forRoot(),
--- src/app/evenementen-bewerken/evenementen-bewerken.component.ts
+++ src/app/evenementen-bewerken/evenementen-bewerken.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
 import {Gebruiker} from "../model/gebruiker";
 import {ActivatedRoute} from "@angular/router";
@@ -21,7 +21,8 @@ export class EvenementenBewerkenComponent implements OnInit {
   ingeschrevenenFLO: FirebaseListObservable<any[]>;
   inschrijvingen: InschrijvingViewModel[] = [];
 
-  constructor(private afdb: AngularFireDatabase, private route: ActivatedRoute) { }
+  constructor(private afdb: AngularFireDatabase, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.gebruikers = this.afdb.list('/gebruikers');
@@ -31,17 +32,30 @@ export class EvenementenBewerkenComponent implements OnInit {
     });
   }
 
+  public schrijfIn(id: string) {
+    if (this.id) {
+      this.afdb.object('/activiteiten/' + this.id + '/ingeschrevenen').update({[id]: false});
+    }
+  }
+
   private mapIngeschrevenen(): void {
     this.ingeschrevenenFLO = this.afdb.list('/activiteiten/' + this.id + '/ingeschrevenen');
     this.ingeschrevenenFLO.subscribe(ingeschrevenen => {
+      this.inschrijvingen = [];
       ingeschrevenen.forEach(ingeschrevene => {
-        this.getGebruiker(ingeschrevene.$key).subscribe(gebruiker => {
-          this.inschrijvingen.push({
-            voornaam: gebruiker.voornaam,
-            achternaam: gebruiker.achternaam,
-            betaald: ingeschrevene.$value,
-          });
-        });
+        const gebruikerSubscription = this.getGebruiker(ingeschrevene.$key).subscribe(
+          (gebruiker) => {
+            this.inschrijvingen.push({
+              voornaam: gebruiker.voornaam,
+              achternaam: gebruiker.achternaam,
+              betaald: ingeschrevene.$value,
+            });
+            gebruikerSubscription.unsubscribe();
+          },
+          (error) => {},
+          () => {
+          }
+          );
       });
     });
   }
