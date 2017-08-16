59e54bffb620b0a960665001fc07db4eea548686
Wim Hambrouck <wim.hambrouck@gmail.com>
2017-08-16 22:24:55 +0200

    gebruiker verwijderen werkt

--- src/app/gebruikers/gebruikers.component.ts
+++ src/app/gebruikers/gebruikers.component.ts
@@ -19,7 +19,7 @@ export class GebruikersComponent implements OnInit {
   gebruikers: FirebaseListObservable<Gebruiker[]>;
   gebruikerAanmakenModal: NgbModalRef;
   gebruikerAanmakenFormGroup: FormGroup;
-  teVerwijderenGebruikerId: string;
+  teVerwijderenGebruikerUid: string;
   teVerwijderenGebruiker: FirebaseObjectObservable<Gebruiker>;
   verwijderGebruikerModal: NgbModalRef;
   alert: IAlert;
@@ -70,14 +70,9 @@ export class GebruikersComponent implements OnInit {
       this.afAuth.auth.createUserWithEmailAndPassword(model.email, 'default')
         .then(gebruiker => {
           // firebase uid van aangemaakte gebruiker op object setten
-          model.id = gebruiker.uid;
+          model.uid = gebruiker.uid;
           // gebruiker met fb uid naar database schrijven
-          this.gebruikers.update(gebruiker.uid, {
-            uid: model.id,
-            voornaam: model.voornaam,
-            achternaam: model.achternaam,
-            saldo: 0
-          });
+          this.gebruikers.update(gebruiker.uid, model);
           this.sluitGebruikerAanmakenModal();
           this.gebruikerAanmakenFormGroup.reset();
           this.disableAanmakenForm = false;
@@ -92,37 +87,61 @@ export class GebruikersComponent implements OnInit {
     }
   }
 
-  openGebruikerVerwijderenModel(content, id: string) {
-    this.teVerwijderenGebruikerId = id;
-    this.teVerwijderenGebruiker = this.afdb.object('/gebruikers/' + id);
+  openGebruikerVerwijderenModel(content, uid: string) {
+    this.teVerwijderenGebruikerUid = uid;
+    this.teVerwijderenGebruiker = this.afdb.object('/gebruikers/' + uid);
     this.verwijderGebruikerModal = this.modalService.open(content);
   }
 
   sluitVerwijderGebruikerModal() {
-    this.teVerwijderenGebruikerId = '';
+    this.teVerwijderenGebruikerUid = '';
     this.teVerwijderenGebruiker = null;
     this.verwijderGebruikerModal.close();
   }
 
-  verwijderGebruikerEnSluit(id: string) {
-    this.gebruikerVerwijderen(id);
+  verwijderGebruikerEnSluit(uid: string) {
+    this.verwijderGebruiker(uid);
     this.sluitVerwijderGebruikerModal();
   }
 
-  gebruikerVerwijderen(id: string) {
-    this.gebruikers.remove(id).then(
-      () => {
-        this.alert = {
-          type: 'success',
-          message: 'De gebruiker werd succesvol verwijderd.'
-        };
-      },
-      error => {
-        this.alert = {
-          type: 'danger',
-          message: 'De gebruiker kon niet worden verwijderd. (' + error.name + ': ' + error.message + ')'
-        };
-      },
-    );
+  /**
+   * Verwijderd een gebruiker uit Firebase met behulp van de deleteUser Cloud Function
+   * Noot: de facto zal de gebruiker ook uit de database worden verwijderd, omdat het deleten van een Firebase-
+   * gebruiker automatisch de Cloud Function deleteUserFromDB doet uitvoeren
+   *
+   * @param {string} uid Het Firebase UID van de te verwijderen gebruiker
+   */
+  verwijderGebruiker(uid: string) {
+    let that = this;
+    //token van aangemelde gebruiker ophalen
+    this.afAuth.auth.currentUser.getToken(true)
+      .then(function (token) {
+        //send token to backend
+        that.httpClient.post(
+          'https://us-central1-dzwelg-dev.cloudfunctions.net/deleteUser',   // url van cloud function
+          null,                                                           // wordt niet gebruikt
+          {
+            params: new HttpParams().set('idToken', token).set('uid', uid)      // parameters voor de function (idToken en UID)
+          })
+          .subscribe((data) => {
+            console.log("GELUKT");
+            // hoewel er data wordt opgevangen en er een bericht wordt gestuurd vanuit de function,
+            // krijgt ik dit niet mee met de data hier... (evt checken op http 200, maar dat lijkt me redundant, omdat
+            // de volgende regels errors opvangt)
+            console.log(data);
+            that.alert = {
+              type: 'info',
+              message: 'Gebruiker zal worden verwijderd...\n(het tot een minuut duren eer de gebruiker effectief verwijderd is)'
+            }
+          }, (fout) => {
+            console.log("FOUT");
+            // foutbericht uit de callback halen
+            console.log(fout.error.message);
+            that.alert = {
+              type: 'danger',
+              message: 'De gebruiker kon niet worden verwijderd. (' + fout.error.message + ')'
+            };
+          });
+      })
   }
 }
