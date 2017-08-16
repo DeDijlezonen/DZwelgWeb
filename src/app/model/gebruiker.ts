3364acb12cfb9abf7de5e7767195cb2c687d0334
Wim Hambrouck <wim.hambrouck@gmail.com>
2017-08-16 22:24:16 +0200

    id en email in gebruiker zijn deprecated

--- src/app/model/gebruiker.ts
+++ src/app/model/gebruiker.ts
@@ -1,7 +1,16 @@
 export interface Gebruiker {
-    id: string;
+  /**
+   * @deprecated Vanaf nu wordt UID gebruikt, gezien dit het UID is van de gebruiker in het Firebase controlepaneel.
+   * id bestaat nog voor compatibiliteit, maar wordt niet meer gebruikt!
+   */
+  id: string;
+  uid: string;
     voornaam: string;
     achternaam: string;
-    email: string;
+  /**
+   * @deprecated Gezien gebruikers nu Firebase gebruikers zijn, staat hun emailadres in het Firebase controlepaneel
+   * en is het niet meer nodig deze in de database op te slagen
+   */
+  email: string;
     saldo: number;
 }
