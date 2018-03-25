87de679223685e192aed2da68307c782de52c2de
Wim Hambrouck <wim.hambrouck@gmail.com>
2018-03-25 23:49:52 +0200

    bugfix: uitschrijven verwijderd saldo niet (#46)

--- src/app/evenementen-bewerken/evenementen-bewerken.component.ts
+++ src/app/evenementen-bewerken/evenementen-bewerken.component.ts
@@ -108,9 +108,10 @@ export class EvenementenBewerkenComponent implements OnInit {
   public schrijfIn(id: string) {
     if (this.id) {
       this.afdb.object('/activiteiten/' + this.id + '/ingeschrevenen').update({[id]: false});
-      this.getEvenement().subscribe((evenement) => {
+      let evt = this.getEvenement().subscribe((evenement) => {
         this.afdb.object('/activiteiten/' + this.id + '/saldi').update({[id]: evenement.tegoed });
       });
+      evt.unsubscribe();
     }
   }
 
